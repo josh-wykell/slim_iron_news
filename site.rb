b7312d531cb8e725a00a8528848ea8b5985c9ed0
Josh Wykell <jwykell@gmail.com>
2015-05-19 15:00:23 -0400

    sass starting to work

--- site.rb
+++ site.rb
@@ -13,13 +13,18 @@ server = WEBrick::HTTPServer.new(:Port => PORT)
 server.mount '/assets', WEBrick::HTTPServlet::FileHandler, "#{ROOT}/public"
 
 server.mount_proc '/' do |req, res|
-  data = YAML.load_file('data.yml')
+  data = YAML.load_file('slim_iron_news.yml')
   template = Tilt.new("#{ROOT}/index.slim")
   res.body = template.render(self, {:data => data})
 end
 
+server.mount_proc '/stylesheets.css' do |req, res|
+  res.body = Tilt.new("#{ROOT}/stylesheets.sass").render
+end
+
+
 trap 'INT' do
-  serv.shutdown
+  server.shutdown
 end
 
 server.start
